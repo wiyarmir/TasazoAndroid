4074ad27956e3c50e13abcdf3ac61d90591191d8
Guillermo <wiyarmir@gmail.com>
2012-04-23 22:17:23 +0200

    Signed-off-by: Guillermo <wiyarmir@gmail.com>

--- CalculadoraTasazo/src/com/wosoft/tasazo/CalculadoraTasazoActivity.java
+++ CalculadoraTasazo/src/com/wosoft/tasazo/CalculadoraTasazoActivity.java
@@ -1,13 +1,41 @@
 package com.wosoft.tasazo;
 
 import android.app.Activity;
+import android.app.Dialog;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class CalculadoraTasazoActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+
+	@Override
+	public boolean onMenuItemSelected(int featureId, MenuItem item) {
+
+		switch (item.getItemId()) {
+		case R.id.menu_mas:
+			// app icon in action bar clicked; go home
+			Dialog d = new Dialog(this, R.style.InfoDialog);
+			d.setTitle(R.string.infoheader);
+			d.setContentView(R.layout.dialog);
+
+			d.show();
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.main_activity, menu);
+		return true;
+	}
 }
