5f16712250969ecdf0ad1ea3b20d2d6b63c4ac41
Guillermo <wiyarmir@gmail.com>
2012-04-24 02:05:18 +0200

    Signed-off-by: Guillermo <wiyarmir@gmail.com>

--- CalculadoraTasazo/src/com/wosoft/tasazo/CalculadoraTasazoActivity.java
+++ CalculadoraTasazo/src/com/wosoft/tasazo/CalculadoraTasazoActivity.java
@@ -1,18 +1,45 @@
 package com.wosoft.tasazo;
 
 import android.app.Activity;
-import android.app.Dialog;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class CalculadoraTasazoActivity extends Activity {
+	private Button bCalcular;
+
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		if (Build.VERSION.SDK_INT > Build.VERSION_CODES.HONEYCOMB) {
+			setTheme(android.R.style.Theme_Holo);
+		}
 		setContentView(R.layout.main);
+		bCalcular = (Button) findViewById(R.id.calcular);
+		bCalcular.setOnClickListener(new View.OnClickListener() {
+
+			public void onClick(View v) {
+				AlertDialog d = (new AlertDialog.Builder(
+						CalculadoraTasazoActivity.this))
+						.setTitle(R.string.infoheader)
+						.setMessage(R.string.infotext).setCancelable(true)
+						.setNeutralButton("Cerrar", new OnClickListener() {
+							public void onClick(DialogInterface dialog,
+									int which) {
+								dialog.dismiss();
+
+							}
+						}).create();
+			}
+		});
 	}
 
 	@Override
@@ -20,10 +47,16 @@ public class CalculadoraTasazoActivity extends Activity {
 
 		switch (item.getItemId()) {
 		case R.id.menu_mas:
-			// app icon in action bar clicked; go home
-			Dialog d = new Dialog(this, R.style.InfoDialog);
-			d.setTitle(R.string.infoheader);
-			d.setContentView(R.layout.dialog);
+
+			AlertDialog d = (new AlertDialog.Builder(this))
+					.setTitle(R.string.infoheader)
+					.setMessage(R.string.infotext).setCancelable(true)
+					.setNeutralButton("Cerrar", new OnClickListener() {
+						public void onClick(DialogInterface dialog, int which) {
+							dialog.dismiss();
+
+						}
+					}).create();
 
 			d.show();
 			return true;
@@ -38,4 +71,5 @@ public class CalculadoraTasazoActivity extends Activity {
 		inflater.inflate(R.menu.main_activity, menu);
 		return true;
 	}
+
 }
