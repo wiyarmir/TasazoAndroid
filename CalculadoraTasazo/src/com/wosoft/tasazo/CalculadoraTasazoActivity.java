15cb103938c795a5529eea0ecdeab708e9cfe570
Guillermo <wiyarmir@gmail.com>
2012-04-24 23:39:11 +0200

    Release
    Signed-off-by: Guillermo <wiyarmir@gmail.com>

--- CalculadoraTasazo/src/com/wosoft/tasazo/CalculadoraTasazoActivity.java
+++ CalculadoraTasazo/src/com/wosoft/tasazo/CalculadoraTasazoActivity.java
@@ -1,5 +1,7 @@
 package com.wosoft.tasazo;
 
+import java.text.DecimalFormat;
+
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
@@ -52,66 +54,76 @@ public class CalculadoraTasazoActivity extends Activity {
 							}
 						}).create();
 
-				int precioCredito = rGrado.isChecked() ? 102 : 238;
-				float min1m = Float.parseFloat(eC1M.getText().toString()
-						.equals("") ? "0" : eC1M.getText().toString())
-						* 0.15f * precioCredito;
-				float max1m = Float.parseFloat(eC1M.getText().toString()
-						.equals("") ? "0" : eC1M.getText().toString())
-						* 0.25f * precioCredito;
-				float min2m = Float.parseFloat(eC2M.getText().toString()
-						.equals("") ? "0" : eC2M.getText().toString())
-						* 0.3f * precioCredito;
-				float max2m = Float.parseFloat(eC2M.getText().toString()
-						.equals("") ? "0" : eC2M.getText().toString())
-						* 0.4f * precioCredito;
-				float min3m = Float.parseFloat(eC3M.getText().toString()
-						.equals("") ? "0" : eC3M.getText().toString())
-						* 0.65f * precioCredito;
-				float max3m = Float.parseFloat(eC3M.getText().toString()
-						.equals("") ? "0" : eC3M.getText().toString())
-						* 0.75f * precioCredito;
-				float min4m = Float.parseFloat(eC4M.getText().toString()
-						.equals("") ? "0" : eC4M.getText().toString())
-						* 0.9f * precioCredito;
-				float max4m = Float.parseFloat(eC4M.getText().toString()
-						.equals("") ? "0" : eC4M.getText().toString())
-						* precioCredito;
 				StringBuilder sb = new StringBuilder();
-				sb.append("Coste total de créditos de primera matrícula:");
-				sb.append("\nCoste mínimo:");
-				sb.append(min1m);
-				sb.append("€");
-				sb.append("\nCoste máximo:");
-				sb.append(max1m);
-				sb.append("€");
-				sb.append("\nCoste total de créditos de segunda matrícula:");
-				sb.append("\nCoste mínimo:");
-				sb.append(min2m);
-				sb.append("€");
-				sb.append("\nCoste máximo:");
-				sb.append(max2m);
-				sb.append("€");
-				sb.append("\nCoste total de créditos de tercera matrícula:");
-				sb.append("\nCoste mínimo:");
-				sb.append(min3m);
-				sb.append("€");
-				sb.append("\nCoste máximo:");
-				sb.append(max3m);
-				sb.append("€");
-				sb.append("\nCoste total de créditos de cuarta matrícula:");
-				sb.append("\nCoste mínimo:");
-				sb.append(min4m);
-				sb.append("€");
-				sb.append("\nCoste máximo:");
-				sb.append(max4m);
-				sb.append("€");
+				DecimalFormat df = new DecimalFormat("0.00");
+				int precioCredito = rGrado.isChecked() ? 102 : 238;
+				float min1m = 0;
+				float max1m = 0;
+				if (!eC1M.getText().toString().equals("")) {
+					min1m = Float.parseFloat(eC1M.getText().toString()) * 0.15f
+							* precioCredito;
+					max1m = Float.parseFloat(eC1M.getText().toString()) * 0.25f
+							* precioCredito;
+					sb.append("Coste total de créditos de primera matrícula:");
+					sb.append("\nCoste mínimo:");
+					sb.append(df.format(min1m));
+					sb.append("€");
+					sb.append("\nCoste máximo:");
+					sb.append(df.format(max1m));
+					sb.append("€");
+				}
+				float min2m = 0;
+				float max2m = 0;
+				if (!eC2M.getText().toString().equals("")) {
+					min2m = Float.parseFloat(eC2M.getText().toString()) * 0.3f
+							* precioCredito;
+					max2m = Float.parseFloat(eC2M.getText().toString()) * 0.4f
+							* precioCredito;
+					sb.append("\nCoste total de créditos de segunda matrícula:");
+					sb.append("\nCoste mínimo:");
+					sb.append(df.format(min2m));
+					sb.append("€");
+					sb.append("\nCoste máximo:");
+					sb.append(df.format(max2m));
+					sb.append("€");
+				}
+				float min3m = 0;
+				float max3m = 0;
+				if (!eC3M.getText().toString().equals("")) {
+					min3m = Float.parseFloat(eC3M.getText().toString()) * 0.65f
+							* precioCredito;
+					max3m = Float.parseFloat(eC3M.getText().toString()) * 0.75f
+							* precioCredito;
+					sb.append("\nCoste total de créditos de tercera matrícula:");
+					sb.append("\nCoste mínimo:");
+					sb.append(df.format(min3m));
+					sb.append("€");
+					sb.append("\nCoste máximo:");
+					sb.append(df.format(max3m));
+					sb.append("€");
+				}
+				float min4m = 0;
+				float max4m = 0;
+				if (!eC4M.getText().toString().equals("")) {
+					min4m = Float.parseFloat(eC4M.getText().toString()) * 0.9f
+							* precioCredito;
+					max4m = Float.parseFloat(eC4M.getText().toString())
+							* precioCredito;
+
+					sb.append("\nCoste total de créditos de cuarta matrícula:");
+					sb.append("\nCoste mínimo:");
+					sb.append(df.format(min4m));
+					sb.append("€");
+					sb.append("\nCoste máximo:");
+					sb.append(df.format(max4m));
+					sb.append("€");
+				}
 				sb.append("\nCoste total de créditos:");
 				sb.append("\nCoste mínimo:");
-				sb.append(min1m + min2m + min3m + min4m);
+				sb.append(df.format(min1m + min2m + min3m + min4m));
 				sb.append("€");
 				sb.append("\nCoste máximo:");
-				sb.append(max1m + max2m + max3m + max4m);
+				sb.append(df.format(max1m + max2m + max3m + max4m));
 				sb.append("€");
 				d.setMessage(sb.toString());
 				d.show();
